70eebe3c3f449f468c601005941adf5489711c45
Maria Vitória <mavxsa@gmail.com>
2025-04-30 10:16:06 -0300

    resolução desafio de controle de fluxo.

--- DesafioControleFluxo/Contador.java
+++ DesafioControleFluxo/Contador.java
@@ -5,31 +5,32 @@ import java.util.Scanner;
 
 public class Contador {
 
-	public static int main(String[] args) {
-
+	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US); 
 
-		Scanner terminal = new Scanner(System.in);
 		System.out.println("Digite o primeiro parâmetro");
 		int parametroUm = scanner.nextInt();
+
 		System.out.println("Digite o segundo parâmetro");
 		int parametroDois = scanner.nextInt();
 		
 		try {
-
-            private static int contar(parametroUm, parametroDois){
-                return parametroDois - parametroUm;
-
-            }
-		
-		}catch (UnsupportedOperationException e) {
-			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+				contar(parametroUm, parametroDois);
+				}catch (ParametrosInvalidosException e) {
+			System.out.println(e.getMessage());
 		}
 		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		
+		if (parametroUm>parametroDois){
+			throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+
+		}
+
 		int contagem = parametroDois - parametroUm;
-		//realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
 	}
+}
 }
